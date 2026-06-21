0be5f1028eecbded1ba37bec5a9fa0a15acbc9de
suil0304 <eagle030410@gmail.com>
2026-06-22 08:25:34 +0900

    fix: VITE_API_URL 환경 변수 값을 JSON 문자열로 변환

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    "import.meta.env.VITE_API_URL": process.env.VITE_API_URL ?? "http://localhost:3000"
+    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL ?? "http://localhost:3000")
   }
 })
